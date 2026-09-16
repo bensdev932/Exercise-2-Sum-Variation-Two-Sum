58babca14ad442f292d2f4c30147eb5a992f43cf
Ben Soeram Ambolodto <bensdev932@gmail.com>
2026-09-16 21:24:51 +0800

    Add project files.

---
+++ Exercise 2 Sum Variation Two Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] nums = [-1, 0];
+int target = -1;
+
+int[] result = TwoSumSorted(nums, target);
+Console.WriteLine($"Positions: {result[0]}, {result[1]}"); 
+
+int[] TwoSumSorted(int[] nums, int target)
+{
+    int left = 0;
+    int right = nums.Length - 1;
+
+    while (left < right)
+    {
+        int sum = nums[left] + nums[right];
+
+        if (sum == target)
+        {
+            
+            return new int[] { left + 1, right + 1 };
+        }
+        else if (sum < target)
+        {
+            left++;
+        }
+        else
+        {
+           
+            right--;
+        }
+    }
+
+    return new int[] { -1, -1 };
+}
